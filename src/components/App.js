1fe892553dd19c7bd5aecd5872070f5acad391b2
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-09-20 14:17:38 +0430

    modify files

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import type {Node} from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+    return <Text>Tayebeh Safdari</Text>;
+}
+
+export default App;
+
