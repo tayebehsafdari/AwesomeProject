840fcda83a4ca90411e5b09ba1e787e0cd9e5497
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-09-20 14:27:47 +0430

    modify files

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import type {Node} from 'react';
 import {Text} from 'react-native';
-import Login from "./Login";
 
-const App = () => {
-    return <Login/>;
+const Login = () => {
+    return <Text>Tayebeh Safdari Doost</Text>;
 }
 
-export default App;
+export default Login;
 
