e9c8ae14d390643d0dc637d7ea71d6ef3aca9c32
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-09-20 15:27:54 +0430

    modify files

---
+++ src/assets/css/login.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    text: {
+        fontSize: 48,
+        color: 'red',
+        borderWidth: 4,
+        borderColor: 'blue',
+        borderStyle: 'solid'
+    }
+});
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import type {Node} from 'react';
 import {Text} from 'react-native';
+import {styles} from './../assets/css/login';
 
 const Login = () => {
-    return <Text>Tayebeh Safdari Doost</Text>;
+    return <Text style={styles.text}>Tayebeh Safdari Doost</Text>;
 }
 
 export default Login;
