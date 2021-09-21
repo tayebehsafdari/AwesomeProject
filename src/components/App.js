f5610a6bdaa4a230ece2c71bb3d502807c899aa4
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-09-21 22:35:55 +0430

    modify files

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import type {Node} from 'react';
-import {Text} from 'react-native';
+import {Text, I18nManager} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from "../pages/Home";
 import Article from "../pages/Article";
 import OriginalStyle from './../assets/css';
+
+I18nManager.forceRTL(true);
 
 const Stack = createNativeStackNavigator();
 const App: ()  =>Node = () => {
